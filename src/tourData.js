78b2c1dff1bf9dc8841007fab4c98bfa7eae8eba
Fat-Tony <fat_tonyy@icloud.com>
2019-11-11 11:50:02 +0000

    initial commit

---
+++ src/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Tamale",
+    img: "./img/2014_Mole.jpg",
+    name: "Mole National Park",
+    info:
+      "Mole National Park has the widest range of wildlife in Ghana and is the only place in Ghana where you can experience elephants in the wild. In addition to the elephants, you will see antelope, bushbucks, monkeys, warthogs, baboons and other smaller wildlife."
+  },
+  {
+    id: 2,
+    city: "Accra",
+    img: "./img/Accra-1.jpg",
+    name: "Capital City Of Ghana",
+    info:
+      "Accra is the capital of Ghana, on the Atlantic coast of West Africa. Kwame Nkrumah Memorial Park honors Ghana’s first president, who helped lead the country to independence. The park contains Nkrumah’s mausoleum and a museum charting his life. Makola Market is the city’s vast, colorful bazaar. Popular seafront spots Labadi Beach and Kokrobite Beach offer golden sand and high-energy nightlife."
+  },
+  {
+    id: 3,
+    city: "Kintampo",
+    img: "./img/Kintampo-waterfalls.jpg",
+    name: "Kintampo-WaterFalls",
+    info:
+      "Kintampo waterfalls is one of the highest waterfalls in Ghana. Also known as Sanders Falls during the colonial days, it is located on the Pumpum river, a tributary of the Black Volta, about 4 kilometres north of Kintampo municipality, on the Kumasi–Tamale road"
+  },
+  {
+    id: 4,
+    city: "Kumasi",
+    img: "./img/Railway.jpg",
+    name: "Kumasi-Accra Railway",
+    info:
+      "The Eastern Line covers a distance of 300 km from Accra to Kumasi with a branch line from Accra to Tema. Apart from the 20 km Accra-Tema Line and the 40 km Accra-Nsawam Line, which are all used for passenger services, the rest of the line is in a state disrepair and inoperative."
+  }
+];
